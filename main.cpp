90ec55eaf57b8067588875a09045017c46f0454d
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-26 12:27:54 -0400

    Can now display only the first and last element

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(void){
     for(int i = 0; i < 10; i++){
         fourDigit[i] = rand() % 9000 + 1000;
         cout << fourDigit[i];
-        if(i < 9) cout << ", ";
+        if(i < 9) cout << " ";
     }
     cout << endl;
     
@@ -26,7 +26,7 @@ int main(void){
     cout << "Even indices: ";
     for(int i = 0; i < 10; i = i + 2){
         cout << fourDigit[i];
-        if(i < 8) cout << ", ";
+        if(i < 8) cout << " ";
     }
     cout << endl;
     
@@ -35,7 +35,7 @@ int main(void){
     for(int i = 0; i < 10; i++){
         if(fourDigit[i] % 2 == 0){
             cout << fourDigit[i];
-            if(i < 9) cout << ", ";
+            if(i < 9) cout << " ";
         }
     }
     cout << endl;
@@ -44,6 +44,12 @@ int main(void){
     cout << "Reversed order: ";
     for(int i = 9; i >= 0; i--){
         cout << fourDigit[i];
-        if(i > 0) cout << ", ";
+        if(i > 0) cout << " ";
     }
+    cout << endl;
+    
+    //first and last element
+    cout << "First and last: " << fourDigit[0] << " " << fourDigit[9] << endl << "Press any key to continue . . ." << endl;
+    
+    
 }
